bf2998a908ed405941d187119085068f7be88f62
wawanindrawan <wawan.jnefer@gmail.com>
2021-10-24 21:06:00 +0800

    pengumpulan tugas 1

---
+++ src/grade/Grade.java
@@ -0,0 +1,25 @@
+package grade;
+
+import java.util.Scanner;
+
+public class Grade {
+
+    public static void main(String[] args) {
+        double nilai;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inputkan nilai : ");
+        nilai = input.nextDouble();
+        if (nilai > 90) {
+            System.out.println("Nilai A");
+        } else if (nilai >= 90) {
+            System.out.println("Nilai B");
+        } else if (nilai >= 80) {
+            System.out.println("Nilai C");
+        } else if (nilai >= 70) {
+            System.out.println("Nilai D");
+        } else {
+            System.out.println("Anda Tidak Lulus");
+        }
+    }
+
+}
